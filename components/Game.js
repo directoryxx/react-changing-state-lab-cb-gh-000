62abd8a2793205e13d2937c4216d65ea6b44f056
Anak Agung Angga Wijaya <anggawijaya4567@gmail.com>
2019-03-04 04:40:24 +0000

    Automatically backed up by Learn

--- components/Game.js
+++ components/Game.js
@@ -7,15 +7,30 @@ export default class Game extends React.Component {
   constructor (props) {
     super(props);
     this.state = {};
+    this.state = {
+      board: [null, null, null, null, null, null, null, null, null],
+      turn: 'X',
+    };
 
     this.handleReset = this.handleReset.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleReset (ev) {
+    ev.preventDefault();
+    this.setState({
+      board: [null, null, null, null, null, null, null, null, null],
+    });
   }
 
   handleClick (i, ev) {
+    ev.preventDefault();
+    const board = this.state.board;
+    board.splice(i, 1, this.state.turn);
+    const turn = this.state.turn === 'X' ? 'O' : 'X';
+    this.setState({
+      board, turn
+    });
   }
 
   getWinner () {
